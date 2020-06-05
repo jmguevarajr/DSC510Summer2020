0db6b0b087001f4b5a20d2919917f285930a29a1
MichaelHotaling <hotalimj@lemoyne.edu>
2020-06-05 09:05:41 -0400

    Assignment 2.1

--- HOTALING_DSC510/cablePriceCalculator.py
+++ HOTALING_DSC510/cablePriceCalculator.py
@@ -1,16 +1,10 @@
-# Programming Assignment 2.1 
-# Michael Hotlaing
-# This programm will:  
-#	1) Welcome the user
-#	2) Retrieve the company name from the user
-#	3) Retrieve the number of feet of fiber optic cable to be installed from the user
-#	4) Calculate the installation cost of fiber optic cable by multiplying
-#	   the total cost as the number of feet times $0.87.
-#	5) Print a receipt for the user including 
-#	   The Company Name
-#	   The Number of Feet of fiber optic cable to be installed
-#	   The Calculated Cost
-#	   The Total Cost
+# Programming Assignment
+# 2.1 Michael Hotaling
+# This programm will: 1) Welcome the user 2) Retrieve the company name
+# from the user 3) Retrieve the number of feet of fiber optic cable to be installed from the user 4) Calculate the
+# installation cost of fiber optic cable by multiplying the total cost as the number of feet times $0.87. 5) Print a
+# receipt for the user including: the Company Name,  the number of feet of fiber optic cable to be installed,
+# the calculated cost, and the total cost
 
 # Defining a Gloabl Variable for the price of fiber in case it ever changes
 fiberPrice = 0.87
@@ -21,12 +15,13 @@ print("Hello! Welcome to the Fiber Optic Price Calculator!")
 # Asking the user which company they are from
 # Convert the company name to a string in case it is something like "101" Cable Oompany
 company = input("Which company are you from?: ")
-print("Okay! You are from " + str(company) +". That's great to hear!" )
+print("Okay! You are from " + str(company) + ". That's great to hear!")
 
 # Asking the user how many feet of cable they will need for their job
 # There is no error handling here, so if a string is passed, it will break the program
 fiberLength = float(input("How many feet of fiber optic cable is required?: "))
-print("Great! You will need " + str(fiberLength) + " feet of fiber optic cable! That will cost you $" + str(fiberLength * fiberPrice))
+print("Great! You will need " + str(fiberLength) + " feet of fiber optic cable! That will cost you $" + str(
+    fiberLength * fiberPrice))
 
 # Some print statements to create some room between the chat and the reciept
 print()
